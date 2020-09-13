b28a3b4a20aa04ed8012134430f19efbb051c50e
Alang Babega <babegamahareza@gmail.com>
2020-09-13 19:00:07 +0700

    SSL Error

--- app.js
+++ app.js
@@ -8,12 +8,6 @@ const app = express();
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.use((req, res, next) => {
-  if (req.get("X-Forwarded-Proto") !== "https") {
-    res.redirect("https://" + req.get("Host") + req.url);
-  } else next();
-});
-
 const url = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_URL}/${process.env.DB_NAME}`;
 mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true });
 
